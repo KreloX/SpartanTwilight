3b133243d753d92124991583aefdf6c03e54f39f
KreloX <31541291+KreloX@users.noreply.github.com>
2022-06-04 12:07:10 +0200

    Release 1.2.0 - Configurable properties

--- src/main/java/krelox/spartantwilight/weaponproperty/SteeleafWeaponProperty.java
+++ src/main/java/krelox/spartantwilight/weaponproperty/SteeleafWeaponProperty.java
@@ -3,6 +3,7 @@ package krelox.spartantwilight.weaponproperty;
 import com.oblivioussp.spartanweaponry.api.ToolMaterialEx;
 import com.oblivioussp.spartanweaponry.api.weaponproperty.WeaponPropertyWithCallback;
 
+import krelox.spartantwilight.handler.ConfigHandlerST;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
@@ -11,16 +12,17 @@ import twilightforest.world.TFWorld;
 
 public class SteeleafWeaponProperty extends WeaponPropertyWithCallback
 {
+	private static final int TIMER = ConfigHandlerST.steeleaf_timer;
+	
 	public SteeleafWeaponProperty(String propType, String propModId) 
 	{
         super(propType, propModId);
     }
 	
-	@Override
 	public void onItemUpdate(ToolMaterialEx material, ItemStack stack, World world, EntityLivingBase entity, int itemSlot, boolean isSelected)
 	{
 		if (TFWorld.isTwilightForest(world))
-			if(!world.isRemote && stack.isItemDamaged() && entity instanceof EntityPlayer && entity.ticksExisted % 200 == 0)
+			if(!world.isRemote && stack.isItemDamaged() && entity instanceof EntityPlayer && entity.ticksExisted % TIMER == 0)
 			{
 				stack.setItemDamage(stack.getItem().getDamage(stack) - 1);
 			}
